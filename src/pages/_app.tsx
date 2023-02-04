3c162f287e6862e13deeaf19f7a7355bf3b205e9
sumi-0011 <selina2000@naver.com>
2023-02-05 03:16:59 +0900

    feat: font 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import GlobalStyle from '@/styles/global-styles';
-import theme from '@/styles/theme';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/global-styles';
+import theme from '@/styles/theme';
+import 'public/fonts/font.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
