40fa75b6df293ba20e15f112af0ff5ec9f0bd46c
sumi-0011 <selina2000@naver.com>
2023-02-05 03:56:44 +0900

    feat: question page ui create

---
+++ src/component/common/CircleButton.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface CircleButtonProps {
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+function CircleButton({ children, onClick }: CircleButtonProps) {
+  return <Wrapper onClick={onClick}>{children}</Wrapper>;
+}
+
+const Wrapper = styled.button`
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.lightGrey};
+  width: 60px;
+  height: 60px;
+`;
+
+export default CircleButton;
--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -1,6 +1,72 @@
+import AButton from '@/component/common/AButton';
+import AText from '@/component/common/AText';
+import CircleButton from '@/component/common/CircleButton';
 import withLayout from '@/component/hoc/withLayout';
+import Image from 'next/image';
+import styled from 'styled-components';
 
 function Question() {
-  return <>Question</>;
+  const handleBackButtonClick = () => {};
+  return (
+    <Wrapper>
+      <div>
+        <Image
+          src="/we-are-The Astronauts.png"
+          width={230}
+          height={52}
+          alt="we-are-The Astronauts"
+        />
+      </div>
+      <div>---------------------------------</div>
+      <div>
+        <div>
+          <Image
+            src="/우주 이미지.png"
+            alt="우주 이미지"
+            width={244}
+            height={244}
+          />
+        </div>
+        <QuestionWrapper>
+          <p> Q1. 나는 우주에 인간 외에</p>
+          <p> 다른 지적 생명체가 존재한다고 생각한다.</p>
+        </QuestionWrapper>
+      </div>
+      <ButtonWrapper>
+        <AButton content="> YES" />
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <AButton content="> NO" />
+      </ButtonWrapper>
+      <CircleButton onClick={handleBackButtonClick}>
+        <Image
+          src={'/back-arrow.png'}
+          width={27}
+          height={23}
+          alt="back-button"
+        />
+      </CircleButton>
+    </Wrapper>
+  );
 }
-export default withLayout(Question, 'test', '우주인 아이디 카드 테스트 중');
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 20px;
+`;
+
+const QuestionWrapper = styled(AText)`
+  color: ${(props) => props.theme.colors.sub2};
+`;
+
+const ButtonWrapper = styled.div`
+  width: 200px;
+`;
+export default withLayout(
+  Question,
+  '우주인 테스트',
+  '우주인 아이디 카드 테스트 중',
+);
