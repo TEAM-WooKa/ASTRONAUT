692b87d99236c148d2b11cb6c516d3e5f6356a96
sumi-0011 <selina2000@naver.com>
2023-03-09 02:54:02 +0900

    feat: seo 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>우주인 ID card</title>
+          <meta name="description" content="We are the astronauts" />
+          <meta
+            name="keywords"
+            content="우주인,swyg,우주인 ID card,메타콘텐츠"
+          />
+
           <meta charSet="utf-8" />
+          <meta name="og:site_name" content="우주인 ID card" />
+          <meta name="og:title" content="우주인 ID card" />
+          <meta name="og:description" content="We are the astronauts" />
+          <meta name="og:type" content="website" />
+          <meta name="og:url" content="https://astronaut-sumi.vercel.app/" />
+          <meta
+            name="og:image"
+            content="https://user-images.githubusercontent.com/49177223/223789248-9cafb888-e552-42c0-92f2-d383364c3026.png"
+          />
+          <meta name="twitter:title" content="우주인 ID card" />
+          {/* Icon Tag */}
+          <link rel="apple-touch-icon" href="/logo.svg" />
+          <link rel="shortcut icon" href="/logo.svg" />
         </Head>
         <body>
           <Main />
