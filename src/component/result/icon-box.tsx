fdf4b116d40a0cd568ef6cb809e153481a6800c5
sumi-0011 <selina2000@naver.com>
2023-03-09 00:09:14 +0900

    feat: 처음으로 돌아가기 기능

--- src/component/result/icon-box.tsx
+++ src/component/result/icon-box.tsx
@@ -2,13 +2,17 @@ import DownloadIcon from '@/assets/icons/DownloadIcon';
 import ReplayIcon from '@/assets/icons/ReplayIcon';
 import ShareIcon from '@/assets/icons/ShareIcon';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
+
 interface IconBoxProps {
   isLoading: boolean;
   onDownloadBtn: () => void;
 }
 
 export default function IconBox({ isLoading, onDownloadBtn }: IconBoxProps) {
+  const router = useRouter();
+
   return (
     <ShareWrapper>
       <span onClick={onDownloadBtn}>
@@ -24,7 +28,9 @@ export default function IconBox({ isLoading, onDownloadBtn }: IconBoxProps) {
         )}
       </span>
       <ShareIcon />
-      <ReplayIcon />
+      <span onClick={() => router.push('/')}>
+        <ReplayIcon />
+      </span>
     </ShareWrapper>
   );
 }
