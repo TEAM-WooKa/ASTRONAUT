e1aa66675d8e2448860e63b3b8d37bc8040d1882
sumi-0011 <selina2000@naver.com>
2023-03-11 22:49:43 +0900

    feat: star 추가

--- src/component/result/IDCard/card-middle/user-middle.tsx
+++ src/component/result/IDCard/card-middle/user-middle.tsx
@@ -21,18 +21,54 @@ export default function UserMiddle({
         <RomiImageWrapper>
           <Img src={character.image} width="60" height="48" alt="image" />
         </RomiImageWrapper>
-        <Img
-          src={image}
-          width="90"
-          height="120"
-          alt="user"
-          onError={handleImageError}
-        />
+        <ImageInnerWrapper>
+          <Img
+            src={image}
+            width="90"
+            height="120"
+            alt="user"
+            onError={handleImageError}
+          />
+        </ImageInnerWrapper>
+        <Star1>
+          <Img
+            src="/images/star.png"
+            width="27.64px"
+            height="27.64px"
+            alt="star"
+          />
+        </Star1>
+        <Star2>
+          <Img
+            src="/images/star.png"
+            width="18.43px"
+            height="18.43px"
+            alt="star"
+          />
+        </Star2>
       </ImageWrapper>
       <TextWrapper>{children}</TextWrapper>
     </MiddleWrapper>
   );
 }
+
+const Star2 = styled.div`
+  position: absolute;
+  bottom: -10px;
+  left: -6px;
+`;
+
+const Star1 = styled.div`
+  position: absolute;
+  top: -5px;
+  right: -10px;
+`;
+const ImageInnerWrapper = styled.div`
+  width: 90px;
+  height: 120px;
+  border-radius: 16px;
+  overflow: hidden;
+`;
 
 const Img = styled.img`
   object-fit: cover;
