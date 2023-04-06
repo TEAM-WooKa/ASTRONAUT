6a653068cbbfc4f05c2cff073b4156b0358f6f56
sumi-0011 <selina2000@naver.com>
2023-04-06 11:37:51 +0900

    feat: 일정 시간이 지나면 메시지 없애기, 시간 지나는것 보여주기

--- src/component/result/guide-message/guide-message-overlay.tsx
+++ src/component/result/guide-message/guide-message-overlay.tsx
@@ -9,7 +9,7 @@ export default function GuideMessageSvg() {
     >
       <path
         d="M1 9C1 4.58172 4.58172 1 9 1H216C220.418 1 224 4.58172 224 9V35C224 39.4183 220.418 43 216 43H44.5L38.5 52.5L31.5 43H9C4.58172 43 1 39.4183 1 35V9Z"
-        fill="#F9F9F9"
+        fill="#d082be5a"
         stroke="#D082BE"
       />
     </svg>
--- src/component/result/guide-message/index.tsx
+++ src/component/result/guide-message/index.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import CircleCheckIcon from '@/assets/icons/CircleCheckIcon';
 import XIcon from '@/assets/icons/XIcon';
+import GuideMessageSvg from '@/component/result/guide-message/guide-message-overlay';
 
 export default function GuideMessage() {
-  return (
-    <Wrapper>
-      <CircleCheckIcon />
-      <Text>카드를 터치해 보세요!</Text>
-      <XIcon color="#828282" />
-    </Wrapper>
-  );
+  const [openTime, handleOpenTime] = useState(0);
+  const [isShowMessage, setIsShowMessage] = useState(true);
+
+  const handleCloseMessage = () => {
+    setIsShowMessage(false);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      handleOpenTime((prev) => prev + 2);
+    }, 50);
+
+    if (openTime > 100) {
+      clearInterval(timer);
+      handleCloseMessage();
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [openTime]);
+
+  if (isShowMessage) {
+    return (
+      <Wrapper>
+        <InnerWrapper>
+          <CircleCheckIcon />
+          <Text>카드를 터치해 보세요!</Text>
+          <span onClick={handleCloseMessage}>
+            <XIcon color="#828282" />
+          </span>
+        </InnerWrapper>
+        <Overlay w={openTime}>
+          <GuideMessageSvg />
+        </Overlay>
+      </Wrapper>
+    );
+  }
+  return <></>;
 }
 
 const Wrapper = styled.div`
@@ -21,11 +54,22 @@ const Wrapper = styled.div`
 
   background-image: url('/images/guide-message-container.svg');
   background-repeat: no-repeat;
+`;
 
+const Overlay = styled.div<{ w: number }>`
+  position: absolute;
+  top: 0;
+  width: ${({ w }) => w}%;
+  overflow: hidden;
+`;
+
+const InnerWrapper = styled.div`
+  height: 43px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 14px 7px;
+  padding: 0 14px;
+  border-radius: 10px;
 `;
 
 const Text = styled.span`
